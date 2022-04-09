984483429725d1e1eb520b6bd3e4ecdeef787f07
adhritdc <82810967+adhritdc@users.noreply.github.com>
2022-04-09 12:34:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+leftWristX = ""; 
+leftWristY = "";
+rightWristX = "";
+rightWristY = "";
+song = "";
+InNumberleftWristY = "";
+remove_decimals = "";
+scoreLeftWrist = "";
+scoreRightWrist="";
+
+The_spectre_song="";
+Harry_potter_theme_song="";
+function preload()
+{
+    The_spectre_song = loadSound("Alan-Walker-Spectre(NCS).mp3");
+    Harry_potter_theme_song = loadSound("ncs_HARRYPOTTERTHEME.mp3");
+}
+function setup()
+{
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function draw()
+{
+    image(video,0,0,600,530);
+}
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function modelLoaded() 
+{
+    console.log("PoseNet is initialized");
+}
+function gotPoses(results) 
+{ 
+    if(results.length > 0) 
+    { 
+        scoreRightWrist = results[0].pose.keypoints[10].score; 
+        scoreLeftWrist = results[0].pose.keypoints[9].score; 
+        console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist); 
+        rightWristX = results[0].pose.rightWrist.x; rightWristY = results[0].pose.rightWrist.y; 
+        console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY); 
+        leftWristX = results[0].pose.leftWrist.x; leftWristY = results[0].pose.leftWrist.y; 
+        console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY); 
+
+    } 
+}
